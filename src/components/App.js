97a848ab884ecad27511fd35fc01c0990882a30a
ksanni <ksanni58627>
2022-12-14 19:36:08 +0530

    update1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
-import React, {useEffect, useState} from 'react'
+import React, { useState } from "react";
 import '../styles/App.css';
 
+
+
 const App = () => {
+  const[color,setColor]=useState(false);
+
+const onChangeColor =()=>{
+  setColor(!color); 
+};
+const handleOnChange = (event) => {
+  setText(event.target.value);
+};
 //code here 
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={`${(color === true)? 'blueColor' : 'redColor'}`} >Newton School</p>
+      <button id='button' onClick={onChangeColor}>Change Style</button>
     </div>
   )
 }
